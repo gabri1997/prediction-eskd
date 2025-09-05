2803e5b94cfe78b706af6c9a9c9f53864b641382
gabri1997 <gabrielerosati97@gmail.com>
2025-09-05 18:31:13 +0200

    finished data processing

--- data_analysis.py
+++ data_analysis.py
@@ -52,7 +52,7 @@ print("Colonne Greek:", greek.columns.tolist())
 print("Colonne Valiga:", valiga.columns.tolist())
 
 # Selezione delle colonne rilevanti
-greek_relevant_cols = ['dateofbirth', 'RBdate', 'Lastvisit','Gender1M2F', 'age',  'uprot_gday', 'M', 'E', 'S', 'T', 'C', 'DiastolicbloodpressuremmHg', 'SystolicbloodpressuremmHg', 'AceiYN', 'Fishoil', 'CELLCEPT', 'AZA', 'CsIm']
+greek_relevant_cols = ['VALIGA_CODE','dateofbirth', 'RBdate', 'Lastvisit','Gender1M2F', 'age',  'uprot_gday', 'M', 'E', 'S', 'T', 'C', 'DiastolicbloodpressuremmHg', 'SystolicbloodpressuremmHg', 'AceiYN', 'Fishoil', 'CELLCEPT', 'AZA', 'CsIm', 'ESKDcorretto']
 greek = greek[greek_relevant_cols]
 valiga_relevant_cols = ['VALIGA CODE', 'SEX', 'M', 'E', 'S', 'T', 'C', 'dateAssess', 'systolic', 'Diastolic', 'age', 'outcome', 'Uprot', 'Nb of Bpmeds', 'RAS blockers', 'fish oil', 'Immunotherapies']
 valiga = valiga[valiga_relevant_cols]
@@ -60,12 +60,94 @@ valiga = valiga[valiga_relevant_cols]
 # Voglio creare una nuova colonna in greek chiamata dateAsses che corrisponde alla differenza in giorni tra Lastvisit e RBdate
 greek['dateAssess'] = (pd.to_datetime(greek['Lastvisit'], format="%d/%m/%Y") - pd.to_datetime(greek['RBdate'], format="%d/%m/%Y")).dt.days
 
-print(greek['dateAssess'].head())
+#print(greek['dateAssess'].head())
+
+# Devo uniformare le colonne
+#final_cols = ['Interval', 'Gender', 'Age', 'M', 'E', 'S', 'T', 'C', 'Proteinuria', 'Hypertension', 'Antihypertensive', 'Immunosuppressants', 'FishOil', 'Eskd']
+
+# Ci vogiono delle funzioni per processare le colonne
+def process_gender(df, gender_col):
+    df['gender'] = df[gender_col].map({1: 'M', 2: 'F', 'M': 'M', 'F': 'F'})
+    return df
+def process_hypertension(df, sys_col, dia_col, acei_col=None):
+  
+    df[sys_col] = pd.to_numeric(df[sys_col], errors='coerce')
+    df[dia_col] = pd.to_numeric(df[dia_col], errors='coerce')
+    
+    hypert = (df[sys_col] >= 140) | (df[dia_col] >= 90)
+    
+    if acei_col:
+        hypert = hypert | (df[acei_col] == 'Y')
+    
+    df['Hypertension'] = hypert.astype(int)
+    return df
+
+def process_proteinuria(df, prot_col):
+    df['Proteinuria'] = pd.to_numeric(df[prot_col], errors='coerce')
+    return df
+def process_therapy_greek(df):
+    df['Antihypertensive'] = (df['AceiYN'] == 'Y').astype(int)
+    df['Immunosuppressants'] = ((df['CsIm'] == 'Y') | (df['AZA'] == 'Y') | (df['CELLCEPT'] == 'Y')).astype(int)
+    df['FishOil'] = (df['Fishoil'] == 'Y').astype(int)
+    return df
+def process_therapy_valiga(df):
+    df['Antihypertensive'] = (df['Nb of Bpmeds'] > 0).astype(int)
+    df['Immunosuppressants'] = (df['Immunotherapies'] == 'Y').astype(int)
+    df['FishOil'] = (df['fish oil'] == 'Y').astype(int)
+    return df
+def process_eskd(df, eskd_col):
+    df['Eskd'] = (df[eskd_col] == 1).astype(int)
+    return df   
+
+# Processamento Greek
+greek = process_gender(greek, 'Gender1M2F')
+greek = process_hypertension(greek, 'SystolicbloodpressuremmHg', 'DiastolicbloodpressuremmHg', 'AceiYN')
+greek = process_proteinuria(greek, 'uprot_gday')
+greek = process_therapy_greek(greek)
+greek['Eskd'] = greek['ESKDcorretto'] if 'ESKDcorretto' in greek.columns else 'Absent'
+# Processamento Valiga
+valiga = process_gender(valiga, 'SEX')
+valiga = process_hypertension(valiga, 'systolic', 'Diastolic')
+valiga = process_proteinuria(valiga, 'Uprot')
+valiga = process_therapy_valiga(valiga)
+valiga = process_eskd(valiga, 'outcome')
 
 # Colonne di tipo data in Greek
 date_cols_greek = ['dateofbirth', 'RBdate', 'Lastvisit']
 for col in date_cols_greek:
     greek[col] = pd.to_datetime(greek[col], errors='coerce').dt.strftime('%d/%m/%Y')
+
+greek.rename(columns={
+    'Gender1M2F': 'Gender',
+    'uprot_gday': 'Proteinuria',
+    'DiastolicbloodpressuremmHg': 'Diastolic',
+    'SystolicbloodpressuremmHg': 'Systolic',
+    'AceiYN': 'Antihypertensive',
+    'Fishoil': 'FishOil',
+    'CELLCEPT': 'CELLCEPT',
+    'AZA': 'AZA',
+    'CsIm': 'CsIm',
+    'ESKDcorretto': 'Eskd',
+    'age': 'Age'
+}, inplace=True)
+
+valiga.rename(columns={
+    'VALIGA CODE': 'VALIGA_CODE',
+    'SEX': 'Gender',
+    'Uprot': 'Proteinuria',
+    'systolic': 'Systolic',
+    'Diastolic': 'Diastolic',
+    'Nb of Bpmeds': 'Nb_of_Bpmeds',
+    'RAS blockers': 'RAS_blockers',
+    'fish oil': 'FishOil',
+    'Immunotherapies': 'Immunotherapies',
+    'age': 'Age'
+}, inplace=True)
+
+last_cols_to_keep = ['VALIGA_CODE','Gender', 'Age', 'dateAssess', 'Hypertension', 'M', 'E', 'S', 'T', 'C', 'Proteinuria', 'Antihypertensive', 'Immunosuppressants', 'FishOil', 'Eskd']
+
+greek = greek[last_cols_to_keep]
+valiga = valiga[last_cols_to_keep]
 
 if os.path.exists(out_greek_path):
     print(f"Il file {out_greek_path} esiste già. Non verrà sovrascritto.")
@@ -81,7 +163,3 @@ else:
 
 print('Colonne greek: ',greek.columns)
 print('Colonne valiga: ',valiga.columns)
- 
-# Devo uniformare le colonne
-final_cols = ['Interval', 'Gender', 'Age', 'M', 'E', 'S', 'T', 'C', 'Proteinuria', 'Hypertension', 'Antihypertensive', 'Immunosuppressants', 'FishOil', 'Eskd']
-
