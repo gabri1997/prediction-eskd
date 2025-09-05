7be0db461da92db07ae147eb60fdb062f811d71b
gabri1997 <gabrielerosati97@gmail.com>
2025-09-05 16:15:35 +0200

    data cleaned

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,6 @@
 """
 1. As first step, the dataset must be cleaned:
- The columns that must be mainteined are : Age, Sex, Hypertension (derivable from systolic and diastolic), MEST-C, Proteinuria and Therapy
+ The columns that must be mainteined are : Age, Gender, Hypertension (derivable from systolic and diastolic), MEST-C, Proteinuria and Therapy
  For what concern Hypertension: following paper hints, if systolic >= 140 mm Hg OR diastolic >= 90 mm Hg or patients receive antihypertensive drugs we can set the value of Hypertension to 1, 0 instead.
  For what concern Proteinuria, in the input files are reported proteinuria values starting from the first then to the last visit, we can simply use the first one registered during the RBdate (renal biopsy date).
  For what concern Therapy, we have to understand the type of treatment that have the patient. Specifically, we can map the different features in the 2 excels to some common features name.
@@ -11,6 +11,39 @@
     Supplementi (olio di pesce)	: (fish oil - Fishoil)
     Intervento chirurgico	Tonsillectomy	—
 
-There is just one column that can't be mapped beacuse it is absent in one of the Greek.xlsx file, that is the Tonsillectomy feature, it indicates if the patient encountered that specific medical surgery.
+There is just one column that can't be mapped because it is absent in one of the Greek.xlsx file, that is the Tonsillectomy feature, it indicates if the patient encountered that specific medical surgery.
+
+Then I would like to have something like:
+
+Code|Gender|Age|AssesDate|Hypertesnion|M|E|S|T|C|Proteinuria|Antihypertensive|Immunosuppressants|FishOil
+
+where Antihypertensive|Immunosuppressants|FishOil are the Therapy 
 
 """
+
+# Pulizia di Greek
+import pandas as pd
+import os
+
+greek_path = '/work/grana_far2023_fomo/ESKD/Data/greek.xls'
+valiga_path = '/work/grana_far2023_fomo/ESKD/Data/valiga.xlsx'
+
+def read_excel_auto(path):
+    ext = os.path.splitext(path)[-1].lower()
+    if ext == ".xls":
+        return pd.read_excel(path, engine="xlrd")
+    elif ext == ".xlsx":
+        return pd.read_excel(path, engine="openpyxl")
+    else:
+        raise ValueError(f"Formato non supportato: {ext}")
+
+greek = read_excel_auto(greek_path)
+valiga = read_excel_auto(valiga_path)
+
+print("Colonne Greek:", greek.columns.tolist())
+print("Colonne Valiga:", valiga.columns.tolist())
+
+# Selezione delle colonne rilevanti
+greek_relevant_cols = ['dateofbirth', 'RBdate', 'Lastvisit','Gender1M2F', 'age',  'uprot_gday', 'M', 'E', 'S', 'T', 'C', 'DiastolicbloodpressuremmHg', 'SystolicbloodpressuremmHg', 'AceiYN', 'Fishoil', 'CELLCEPT', 'AZA', 'CsIm']
+greek = greek[greek_relevant_cols]
+greek.to_excel('/work/grana_far2023_fomo/ESKD/Data/cleaned_greek.xlsx', index=False)
